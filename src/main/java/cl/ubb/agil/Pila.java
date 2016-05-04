9df21de031f26c6eeec4419a1a74b23b2b232ff3
miguel <mimarti@alumnos.ubiobio.cl>
2016-05-04 11:33:31 -0400

    ingresa uno

--- src/main/java/cl/ubb/agil/Pila.java
+++ src/main/java/cl/ubb/agil/Pila.java
@@ -2,8 +2,10 @@ package cl.ubb.agil;
 
 public class Pila {
 
+	
+	
 	public Pila(){
-		
+		int[] pila = new int[10];	
 	}
 
 	
--- src/test/java/cl/ubb/agil/PilaTest.java
+++ src/test/java/cl/ubb/agil/PilaTest.java
@@ -1,13 +1,20 @@
 package cl.ubb.agil;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class PilaTest {
 
+	
 	@Test
 	public void setup(){
 		Pila pila = new Pila();
 	}
+	
+	
+	
+	
 	
 	
 	
