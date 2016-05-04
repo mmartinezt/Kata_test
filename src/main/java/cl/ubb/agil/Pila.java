b0c5a5bc000328571b587efeb5b19257f1befdde
miguel <mimarti@alumnos.ubiobio.cl>
2016-05-04 11:06:29 -0400

    nuevo stack parte vacia

--- src/main/java/cl/ubb/agil/Pila.java
+++ src/main/java/cl/ubb/agil/Pila.java
@@ -2,4 +2,10 @@ package cl.ubb.agil;
 
 public class Pila {
 
+	public Pila(){
+		
+	}
+
+	
+	
 }
--- src/test/java/cl/ubb/agil/PilaTest.java
+++ src/test/java/cl/ubb/agil/PilaTest.java
@@ -1,5 +1,14 @@
 package cl.ubb.agil;
 
+import org.junit.Test;
+
 public class PilaTest {
 
+	@Test
+	public void setup(){
+		Pila pila = new Pila();
+	}
+	
+	
+	
 }
